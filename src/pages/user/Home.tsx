bfb804982b632ffd31d06288510357e99b31c239
Trí Hoàng <tri1234512321@gmail.com>
2024-11-26 13:25:34 +0700

    install tailwind

--- src/pages/user/Home.tsx
+++ src/pages/user/Home.tsx
@@ -1,5 +1,9 @@
 const UserHome = () => {
-  return <h1>User Home</h1>
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">USER HOME!</h1>
+    </div>
+  );
 }
 
 export default UserHome
